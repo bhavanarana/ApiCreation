d7444f84433cdad34b3837777d7ed19598d3ca80
Bhavana <4bhavanarana@gmail.com>
2023-02-15 10:40:14 +0530

    added json data

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+app.use(cors());
 const port = process.env.PORT || 8000;
+const apiData = require("./data.json");
 app.get("/", (req, res) => {
   res.send("Hello I'm Live");
+});
+app.get("/service", (req, res) => {
+  res.send(apiData);
 });
 app.listen(port, () => {
   console.log("I am Live again");
